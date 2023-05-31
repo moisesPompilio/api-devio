6337cde557e4307206e0ddc13dc4565bacc0feb5
moisesPompilio <moisesjtmed@gmail.com>
2023-05-31 04:19:10 -0300

    docs: document extra with swagger

--- src/modules/extra/adapter/http/rest/extra.controller.ts
+++ src/modules/extra/adapter/http/rest/extra.controller.ts
@@ -9,12 +9,9 @@ import {
 } from '@nestjs/common';
 import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 import { IsFuncionarioRule } from 'src/auth/decorators/is-funcionario-rule.decorator';
-import {
-  ApiOperation,
-  ApiResponse,
-  ApiTags,
-  ApiBearerAuth,
-} from '@nestjs/swagger';
+import { ApiResponse, ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { Extra } from 'src/modules/extra/domain/entities/extra.entity';
+import { typeException } from 'src/shared/exception/type-http.exception';
 import { ExtraService } from '../../../aplication/service/extra.service';
 import { CreateExtraDto } from '../../../aplication/dto/create-extra.dto';
 
@@ -23,8 +20,25 @@ import { CreateExtraDto } from '../../../aplication/dto/create-extra.dto';
 export class ExtraController {
   constructor(private readonly extraService: ExtraService) {}
 
-  @ApiOperation({
-    summary: 'responsible for doing all the CRUD of the extra',
+  @ApiResponse({
+    status: 201,
+    description: 'Save Extra correct and return Extra',
+    type: Extra,
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'internal error when doing the operation',
+    type: typeException,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'request has an error',
+    type: typeException,
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized. access denied',
+    type: typeException,
   })
   @ApiBearerAuth()
   @Post()
@@ -36,12 +50,47 @@ export class ExtraController {
     return this.extraService.create(createExtraDto);
   }
 
+  @ApiResponse({
+    status: 200,
+    description: 'Get extra sucess',
+    type: Extra,
+    isArray: true,
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'internal error when doing the operation',
+    type: typeException,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'request has an error',
+    type: typeException,
+  })
   @Get()
   @IsPublic()
   findAll() {
     return this.extraService.getAll();
   }
 
+  @ApiResponse({
+    status: 200,
+    description: 'upadte extra sucess',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'internal error when doing the operation',
+    type: typeException,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'request has an error',
+    type: typeException,
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized. access denied',
+    type: typeException,
+  })
   @Put(':id')
   @ApiBearerAuth()
   update(
@@ -53,6 +102,25 @@ export class ExtraController {
     return this.extraService.update(id, updateExtraDto);
   }
 
+  @ApiResponse({
+    status: 200,
+    description: 'delete extra sucess',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'internal error when doing the operation',
+    type: typeException,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'request has an error',
+    type: typeException,
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized. access denied',
+    type: typeException,
+  })
   @ApiBearerAuth()
   @Delete(':id')
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
