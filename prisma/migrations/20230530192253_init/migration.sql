e8cc8d3ec21f5671be3ec469b09292d87f07cf68
moisesPompilio <moisesjtmed@gmail.com>
2023-05-30 22:43:53 -0300

    feaut: implementation of the sqlite database, leaving it working with all the correct structure of the tables

--- prisma/migrations/20230530192253_init/migration.sql
+++ prisma/migrations/20230530192253_init/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "name" TEXT NOT NULL,
@@ -8,29 +8,29 @@ CREATE TABLE "User" (
 );
 
 -- CreateTable
-CREATE TABLE "Category" (
+CREATE TABLE "product" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "image" TEXT NOT NULL
 );
 
 -- CreateTable
-CREATE TABLE "Product" (
+CREATE TABLE "category" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "price" INTEGER NOT NULL,
+    "price" REAL NOT NULL,
     "categoryId" TEXT NOT NULL,
     "image" TEXT NOT NULL,
     "code" TEXT NOT NULL,
-    CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "category_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "product" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "Extra" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
-    "price" INTEGER NOT NULL,
+    "price" REAL NOT NULL,
     "image" TEXT NOT NULL,
     "description" TEXT NOT NULL
 );
@@ -40,7 +40,7 @@ CREATE TABLE "_ProductExtra" (
     "A" TEXT NOT NULL,
     "B" TEXT NOT NULL,
     CONSTRAINT "_ProductExtra_A_fkey" FOREIGN KEY ("A") REFERENCES "Extra" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "_ProductExtra_B_fkey" FOREIGN KEY ("B") REFERENCES "Product" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT "_ProductExtra_B_fkey" FOREIGN KEY ("B") REFERENCES "category" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateIndex
