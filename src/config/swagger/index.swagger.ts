84c6a36e342760a70f5bbaf6ed30cc2f40514c7f
moisesPompilio <moisesjtmed@gmail.com>
2023-05-30 22:48:04 -0300

    chore: create swagger config

---
+++ src/config/swagger/index.swagger.ts
@@ -0,0 +1,10 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export const swaggerConfig = new DocumentBuilder()
+  .setTitle('API para teste de hablidade para empresa trakto')
+  .setDescription(
+    'Esse teste foi realizado de acordo com as intruções passadas, ele foi construido com nest.js',
+  )
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
