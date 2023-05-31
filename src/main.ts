08a50d4f9c7f9a5e387efc3d48cf7afb8df8a3ad
moisesPompilio <moisesjtmed@gmail.com>
2023-05-30 23:20:40 -0300

    feat: configuration port api

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 
 bootstrap();
